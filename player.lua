42b52ec162565136e398224ed1043c69ba420623
Garbhan Kenneally <garbhan@mini.local>
2025-01-02 20:42:37 +0000

    uppercase P2 table

--- player.lua
+++ player.lua
@@ -13,14 +13,14 @@ function P1:draw()
     love.graphics.rectangle("fill", P1.x, P1.y, P1.w, P1.h)
 end
 
-p2 = {}
-p2.x = love.graphics.getWidth() - 20
-p2.y = love.graphics.getHeight() / 2
-p2.w = 20
-p2.h = 110
-p2.score = 0
+P2 = {}
+P2.x = love.graphics.getWidth() - 20
+P2.y = love.graphics.getHeight() / 2
+P2.w = 20
+P2.h = 110
+P2.score = 0
 
-function p2:draw()
+function P2:draw()
     love.graphics.setColor(love.math.colorFromBytes(69, 133, 136))
-    love.graphics.rectangle("fill", p2.x, p2.y, p2.w, p2.h)
+    love.graphics.rectangle("fill", P2.x, P2.y, P2.w, P2.h)
 end
